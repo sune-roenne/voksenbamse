4d4061cb5d7b0bfb4647b66db5d11d048296fd39
Sune Rønne <surn@nykredit.dk>
2026-02-22 01:24:12 +0100

    contents

---
+++ VoksenBamse.UI/Pages/Home.razor.cs
@@ -0,0 +1,15 @@
+﻿namespace VoksenBamse.UI.Pages;
+
+public partial class Home
+{
+    private static string[] Quotes = [
+        "Åh nej, skal du til at være ulækker med dine orme, K*lling?",
+        "Nå nå nå nå nå nå nå nå nå!",
+        "Blev der sagt!",
+        "Så så man lige mig!",
+        "Lad mig!"
+
+        ];
+
+
+}

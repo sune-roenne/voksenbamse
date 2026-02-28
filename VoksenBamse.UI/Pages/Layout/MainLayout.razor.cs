b1acd86145ae09097c3ff3e89de13fe5f4639377
Sune Rønne <SURN@nykredit.dk>
2026-02-28 23:34:45 +0100

    fotoda

---
+++ VoksenBamse.UI/Pages/Layout/MainLayout.razor.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+using VoksenBamse.UI.Integration.Conversation;
+using VoksenBamse.UI.ViewModel.Conversation;
+
+namespace VoksenBamse.UI.Pages.Layout;
+
+public partial class MainLayout
+{
+    [Inject]
+    public IConversationLoader Loader { get; set; }
+
+    private IReadOnlyCollection<BamseConversationInfoVmo>? _infos;
+
+    protected override async Task OnParametersSetAsync()
+    {
+        _infos = await Loader.LoadInfos();
+    }
+
+}
--- VoksenBamse.UI/scripts/common.ts
+++ VoksenBamse.UI/scripts/common.ts
@@ -1,8 +1,33 @@
+const sideNavId = 'side-nav'
+const sideNavExpansionButtonId = 'side-nav-expansion-btn'
+const sideNavExpandedClass = 'side-nav-expanded'
+const sideNavCollapsedClass = 'side-nav-collapsed'
+const sideNavExpandButtonIconId = 'side-nav-expansion-icon'
+const sideNavExpandButtonIconExpandedClass = 'bi-arrows-expand-vertical'
+const sideNavExpandButtonIconCollapsedClass = 'bi-arrows-collapse-vertical'
 
 
 document.onreadystatechange = function (ev) {
     if (this.readyState == "interactive") {
-        this.getElementById()
+        const sideNav = this.getElementById(`${sideNavId}`)
+        const expansionButton = this.getElementById(`${sideNavExpansionButtonId}`)
+        const expansionButtonIcon = this.getElementById(`${sideNavExpandButtonIconId}`)
+
+        expansionButton.onclick = function (clkev) {
+            var sideNavIsExpanded = sideNav.classList.contains(sideNavExpandedClass)
+            if (sideNavIsExpanded) {
+                sideNav.classList.remove(sideNavExpandedClass)
+                sideNav.classList.add(sideNavCollapsedClass)
+                expansionButtonIcon.classList.remove(sideNavExpandButtonIconExpandedClass)
+                expansionButtonIcon.classList.add(sideNavExpandButtonIconCollapsedClass)
+            }
+            else {
+                sideNav.classList.remove(sideNavCollapsedClass)
+                sideNav.classList.add(sideNavExpandedClass)
+                expansionButtonIcon.classList.remove(sideNavExpandButtonIconCollapsedClass)
+                expansionButtonIcon.classList.add(sideNavExpandButtonIconExpandedClass)
+            }
+        }
     }
 
 };
